fdb44994c24119f39261387ced2e0e1322cf8b1b
davestroud <david@davidstroud.me>
2016-12-26 10:13:01 -0600

    add Number of Days in Month file

---
+++ Arrays/Play.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  const int SIZE = 5;
+  int values[SIZE];
+  int count;
+
+  for (count = 0; count < 5; count++)
+      values[count] = count +1;
+
+  for (count = 0; count < 5; count++)
+      cout << values[count];
+
+  return 0;
+}
