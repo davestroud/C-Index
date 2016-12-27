dc170f45374cdc5f01eb8f7a32016681ae419f3b
davestroud <david@davidstroud.me>
2016-12-27 17:33:00 -0600

    add UnderflowOverflow file

---
+++ Expressions/Test.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  double x, y = 4;
+  float z = 2.7;
+  x = y * z;
+  cout << x << endl;
+
+  cout << " " << endl;
+
+  double parts;
+  parts = 15.0 / 6;
+  cout << parts << endl;
+
+	return 0;
+}
