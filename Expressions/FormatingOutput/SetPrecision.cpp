94149a6c5f07ec1e65c527500f131cf3231f3f7b
davestroud <david@davidstroud.me>
2016-12-28 09:35:50 -0600

    add setprecision file

---
+++ Expressions/FormatingOutput/SetPrecision.cpp
@@ -0,0 +1,18 @@
+// This program demonstrates how setprecision rounds a
+// floating point value.
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	double quotient, number1 = 132.364, number2 = 26.91;
+
+	quotient = number1 / number2;
+	cout << quotient << endl;
+	cout << setprecision(5) << quotient << endl;
+	cout << setprecision(4) << quotient << endl;
+	cout << setprecision(3) << quotient << endl;
+	cout << setprecision(2) << quotient << endl;
+	cout << setprecision(1) << quotient << endl;
+	return 0;
+}
