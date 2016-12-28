8442e6c2d750f07d015c54a5a42c333e91540e16
davestroud <david@davidstroud.me>
2016-12-28 13:11:37 -0600

    add MathLibrary

--- Expressions/Test.cpp
+++ Expressions/Test.cpp
@@ -1,20 +1,16 @@
-
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-  double x, y = 4;
-  float z = 2.7;
-  x = y * z;
-  cout << x << endl;
-
-  cout << " " << endl;
-
-  double parts;
-  parts = 15.0 / 6;
-  cout << parts << endl;
+  int num;
+  double s;
+  cout << "Enter a number: ";
+  cin >> num;
+  s = sqrt(num);
+  cout << "The square root of " << num << " is " << s << endl;
 
 	return 0;
 }
