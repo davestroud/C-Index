7b53703da0edf9b063239e6a6247f2290f504218
davestroud <david@davidstroud.me>
2016-12-29 10:58:46 -0600

    add MultipleNested file

---
+++ MakingDecisions/NestedIfStatement/MultipleNested.cpp
@@ -0,0 +1,36 @@
+// This program uses nested if/else statements to assign a
+// letter grade (A, B, C, D, or F) to a numeric test score.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	// Constants for grade thresholds
+	const int A_SCORE = 90, B_SCORE = 80, C_SCORE = 70, D_SCORE = 60;
+
+	int testScore;  // To hold the numeric test score
+
+	// Get the numeric test score
+	cout << "Enter your numeric test socre and I will\n";
+	cout << "tell you the letter grade you earned.\n";
+	cin >> testScore;
+
+	if (testScore >= A_SCORE) {
+		cout << "Your grade is A.\n";
+	} else {
+		if (testScore >= B_SCORE) {
+			cout << "Your grade is B.\n";
+		} else {
+			if (testScore >= C_SCORE) {
+				cout << "Your grade is C.\n";
+			} else {
+				if (testScore >= D_SCORE) {
+					cout << "Your grade is D.\n";
+				} else {
+					cout << "Your grad is F.\n";
+				}
+			}
+		}
+	}
+
+	return 0;
+}
