0b366c37e290998e46cc32fd86134f9c8e3bebe2
davestroud <david@davidstroud.me>
2016-12-29 11:13:40 -0600

    add IfElseIF folder

--- MakingDecisions/IfElseIfStatement/Main.cpp
+++ MakingDecisions/IfElseIfStatement/Main.cpp
@@ -1,4 +1,4 @@
-// This program uses nested if/else statements to assign a
+// This program uses nested if/else if statements to assign a
 // letter grade (A, B, C, D, or F) to a numeric test score.
 #include <iostream>
 using namespace std;
@@ -14,23 +14,17 @@ int main(int argc, char const *argv[]) {
 	cout << "tell you the letter grade you earned.\n";
 	cin >> testScore;
 
-	if (testScore >= A_SCORE) {
+	// Determine the letter grade.
+	if (testScore >= A_SCORE)
 		cout << "Your grade is A.\n";
-	} else {
-		if (testScore >= B_SCORE) {
-			cout << "Your grade is B.\n";
-		} else {
-			if (testScore >= C_SCORE) {
-				cout << "Your grade is C.\n";
-			} else {
-				if (testScore >= D_SCORE) {
-					cout << "Your grade is D.\n";
-				} else {
-					cout << "Your grade is F.\n";
-				}
-			}
-		}
-	}
+	else if (testScore >= B_SCORE)
+		cout << "Your grade is B.\n";
+	else if (testScore >= C_SCORE)
+		cout << "Your grade is C.\n";
+	else if (testScore >= D_SCORE)
+		cout << "Your grade is D.\n";
+	else
+		cout << "Your grade is F.\n";
 
 	return 0;
 }
