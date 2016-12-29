a40c5e8222c93dca003025eec58f2afcd2509271
davestroud <david@davidstroud.me>
2016-12-28 19:38:08 -0600

    add SimpleIfStatement

---
+++ MakingDecisions/Play.cpp
@@ -0,0 +1,13 @@
+// This program displays the values of true and false states.
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	bool z, a, b;
+	int x = 5, y = 10;
+
+  cout << (b = y != x) << endl;
+
+	return 0;
+}
