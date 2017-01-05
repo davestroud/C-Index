a4ecd3b2264b8b91c8281a6d6576f6e2ac629697
davestroud <david@davidstroud.me>
2017-01-05 16:04:14 -0500

    add hour loop to Clock

--- LoopsAndFiles/NestedLoops/Clock.cpp
+++ LoopsAndFiles/NestedLoops/Clock.cpp
@@ -6,10 +6,13 @@ int main(int argc, char const *argv[]) {
 
   cout << fixed << right;
   cout.fill('0'); // causes 0 to be printed in front of single digit numbers.
-  for (int minutes = 0; minutes < 60; minutes++) {
-    for (int seconds = 0; seconds < 60; seconds++) {
-      cout << setw(2) << minutes << ":";
-      cout << setw(2) << seconds << endl;
+  for (int hours = 0; hours < 24; hours++) {
+    for (int minutes = 0; minutes < 60; minutes++) {
+      for (int seconds = 0; seconds < 60; seconds++) {
+        cout << setw(2) << hours << ":";
+        cout << setw(2) << minutes << ":";
+        cout << setw(2) << seconds << endl;
+      }
     }
   }
   return 0;
